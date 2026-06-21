ffeb938532733b9c705176e4c8349aa98fcf724f
Mauricio AB <dev.mauricioalezandre@gmail.com>
2026-06-21 20:06:53 -0300

    🐞 wip

--- phase2/ex01_type_hints.py
+++ phase2/ex01_type_hints.py
@@ -3,25 +3,23 @@
 
 
 def full_name(first: str, last: str) -> str:
-    # TODO: return "First Last"
-    pass
+    return f"{first.title()} {last.title()} "
 
 
 def is_adult(age: int) -> bool:
     # TODO: return True if age >= 18
-    pass
-
+    if age >= 18:
+        return True
+    return False
 
 def average(numbers: list[float]) -> float:
-    # TODO: return the average of the list
-    pass
-
+    return sum(numbers) / len(numbers)
 
 def main():
-    print(full_name("Mauricio", "AB"))   # "Mauricio AB"
-    print(is_adult(20))                  # True
-    print(is_adult(15))                  # False
-    print(average([10.0, 20.0, 30.0]))   # 20.0
+    print(full_name("mauricio", "alexandre"))   # "Mauricio Alexandre"
+    print(is_adult(20))                         # True
+    print(is_adult(15))                         # False
+    print(average([10.0, 20.0, 30.0]))          # 20.0
 
 
 main()
